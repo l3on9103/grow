25d01b9ddcef2d37e2e7846448927f9f34c17a2f
Thomas <leonthomas15@gmail.com>
2020-05-14 20:44:30 +0530

    Updated Settins Avtivity

--- app/src/main/java/com/example/grow/SettingsActivity.java
+++ app/src/main/java/com/example/grow/SettingsActivity.java
@@ -3,14 +3,25 @@ package com.example.grow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.helloworld.R;
 
 public class SettingsActivity extends AppCompatActivity {
+    private Button saveBtn;
+    private EditText userNameET, userBioET;
+    private ImageView profileImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        saveBtn = findViewById(R.id.save_settings_btn);
+        userNameET = findViewById(R.id.username_settings);
+        userBioET = findViewById(R.id.bio_settings);
+        profileImageView = findViewById(R.id.settings_profile_image);
     }
 }
