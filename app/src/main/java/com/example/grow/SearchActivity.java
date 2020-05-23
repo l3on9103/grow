ea76e43406e2a65f1fd9cc539f06d537d18256f8
Thomas <leonthomas15@gmail.com>
2020-05-23 20:50:55 +0530

    Search Activity updated, but unable to land on search activity.

--- app/src/main/java/com/example/grow/SearchActivity.java
+++ app/src/main/java/com/example/grow/SearchActivity.java
@@ -1,15 +1,21 @@
 package com.example.grow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.helloworld.R;
 
 public class SearchActivity extends AppCompatActivity {
     private TextView textView;
+    private RecyclerView findFriendList;
+    private EditText searchET;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +23,8 @@ public class SearchActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.welcome_mesage);
         Intent intent = getIntent();
         textView.setText("Welcome " + intent.getStringExtra("USER_NAME"));
+        searchET = findViewById(R.id.search_user_text);
+        findFriendList = findViewById(R.id.find_friends_list);
+        findFriendList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
     }
 }
