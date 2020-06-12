1ee262ff4d0389899b7612cdbebda2aef8fe8eb1
Thomas <leonthomas15@gmail.com>
2020-06-12 12:43:48 +0530

    Contact and Notification design updated

--- app/src/main/java/com/example/grow/NotificationsActivity.java
+++ app/src/main/java/com/example/grow/NotificationsActivity.java
@@ -1,8 +1,15 @@
 package com.example.grow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.helloworld.R;
 
@@ -12,5 +19,21 @@ public class NotificationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+    }
+
+    public static class NotificationsViewHolder extends RecyclerView.ViewHolder {
+        TextView userNameTxt;
+        Button acceptBtn, cancelBtn;
+        ImageView profileImageView;
+        RelativeLayout cardView;
+
+        public NotificationsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userNameTxt = itemView.findViewById(R.id.name_notification);
+            acceptBtn = itemView.findViewById(R.id.request_accept_button);
+            cancelBtn = itemView.findViewById(R.id.request_decline_button);
+            profileImageView = itemView.findViewById(R.id.image_notification);
+            cardView = itemView.findViewById(R.id.card_view);
+        }
     }
 }
--- app/src/main/java/com/example/grow/SearchActivity.java
+++ app/src/main/java/com/example/grow/SearchActivity.java
@@ -1,12 +1,18 @@
 package com.example.grow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.helloworld.R;
@@ -20,11 +26,26 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        textView = (TextView) findViewById(R.id.welcome_mesage);
+        textView = (TextView) findViewById(R.id.welcome_message);
         Intent intent = getIntent();
         textView.setText("Welcome " + intent.getStringExtra("USER_NAME"));
         searchET = findViewById(R.id.search_user_text);
         findFriendList = findViewById(R.id.find_friends_list);
         findFriendList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+
+    public static class FindFriendsViewHolder extends RecyclerView.ViewHolder {
+        TextView userNameTxt;
+        Button videoCallBtn;
+        ImageView profileImageView;
+        RelativeLayout cardView;
+
+        public FindFriendsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userNameTxt = itemView.findViewById(R.id.name_contact);
+            videoCallBtn = itemView.findViewById(R.id.call_button);
+            profileImageView = itemView.findViewById(R.id.image_contact);
+            cardView = itemView.findViewById(R.id.card_view1);
+        }
     }
 }
